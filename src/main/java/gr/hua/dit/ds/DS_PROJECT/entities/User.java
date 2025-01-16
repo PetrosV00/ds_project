5af6085c8694cee4ea2dec4e562af10a1aa8db99
petros <petrosvougiou00@gmail.com>
2025-01-16 22:32:02 +0200

    Field "email" on User entity set to unique constraint

--- src/main/java/gr/hua/dit/ds/DS_PROJECT/entities/User.java
+++ src/main/java/gr/hua/dit/ds/DS_PROJECT/entities/User.java
@@ -14,6 +14,7 @@ import java.util.Set;
 @Table(	name = "users",
         uniqueConstraints = {
                 @UniqueConstraint(columnNames = "username"),
+                @UniqueConstraint(columnNames = "email")
         })
 public class User {
     @Id
